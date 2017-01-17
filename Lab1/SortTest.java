f5391f61cc2c6003bfd4b068820437cd68b6a87a
Alex <agrazela@gmail.com>
2017-01-16 18:37:07 -0800

    Start of the Testing class

--- Lab1/SortTest.java
+++ Lab1/SortTest.java
@@ -1,9 +1,27 @@
 import java.utils.*;
 
-
+public static final ARRLEN = 15;
 public class SortTest {
 
     static int main(String args[]) {
+        ArrayList<int> ControlArray = new ArrayList<int>(ARRLEN);
+        ArrayList<int> SortArray;
+        Random rnd = new Random();
+        int[ARRLEN] arr;
+        
 
+        for (int i = 0; i < ARRLEN; i++) {
+            int rndNum = rnd.nextInt();
+            arr[i] = rndNum;
+            ControlArray.add(rndNum);
+        }
+        selectionSort(arr, ARRLEN);
+        SortArray = new ArrayList<int>(arr);
+        if (SortArray.equals(ControlArray.sort())) {
+            System.out.println("Hurray Selection Sort Worked!\n");
+        }
+        else {
+            System.out.println("Selection Sort DIDN'T Work\n");
+        }
     }
 }
