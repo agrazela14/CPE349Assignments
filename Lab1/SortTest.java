fc95e410e4c98d0097d064ac5c0b4e38f4f2ab9d
Alex <agrazela@gmail.com>
2017-01-16 18:46:48 -0800

    more on the test

--- Lab1/SortTest.java
+++ Lab1/SortTest.java
@@ -8,20 +8,22 @@ public class SortTest {
         ArrayList<int> SortArray;
         Random rnd = new Random();
         int[ARRLEN] arr;
+        System.out.println("The Random Numbers are");
         
 
         for (int i = 0; i < ARRLEN; i++) {
             int rndNum = rnd.nextInt();
             arr[i] = rndNum;
             ControlArray.add(rndNum);
+            System.out.println(rndNum);
         }
         selectionSort(arr, ARRLEN);
         SortArray = new ArrayList<int>(arr);
         if (SortArray.equals(ControlArray.sort())) {
-            System.out.println("Hurray Selection Sort Worked!\n");
+            System.out.println("Hurray Selection Sort Worked!");
         }
         else {
-            System.out.println("Selection Sort DIDN'T Work\n");
+            System.out.println("Selection Sort DIDN'T Work!");
         }
     }
 }
