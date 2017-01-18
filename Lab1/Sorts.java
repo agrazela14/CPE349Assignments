66b2f7cbb08e5186d87e5973344435d3eacbaa26
agrazela14 <agrazela@gmail.com>
2017-01-18 11:09:14 -0800

    Makefile, and some other changes to merge

--- Lab1/Sorts.java
+++ Lab1/Sorts.java
@@ -19,7 +19,7 @@ public class Sorts {
     }
 
     private static void mergeSort(int[] arr, int first, int last) {
-        if (first != last) {
+        if (first < last) {
             int middle = (first + last) / 2;
             mergeSort(arr, first, middle); 
             mergeSort(arr, middle + 1, last); 
@@ -27,12 +27,42 @@ public class Sorts {
         }
     }
 
-    private static void mergeSortedHalves(int[] arr, int left, int middle, int right) {
-        int[] temp = new int[right - left + 1];
-        int index1 = left;
+    private static void mergeSortedHalves(int[] arr, int first, int middle, int last) {
+        int[] temp = new int[arr.length];
+        int index1 = first;
         int index2 = middle + 1;
-        int index = 0;
-        while ((index1 <= middle) && (index2 <= right)) {
+        int index  = first;
+         
+        for (index = first; index < last; index++) {
+            temp[index] = arr[index];
+        }
+        index = first;
+
+        while ((index1 <= middle) && (index2 <= last)) {
+            if (temp[index1 - first] < temp[index2]) {
+                arr[index] = temp[index1];
+                index1++;
+            }
+            else {
+                arr[index] = temp[index2];
+                index2++;
+            }
+            index++;
+        }
+
+        while (index1 <= middle) {
+            arr[index] = temp[index1];
+            index++;
+            index1++;
+        }
+        while (index2 <= last) {
+            arr[index] = temp[index2];
+            index++;
+            index2++;
+        }
+        
+        /*
+        while ((index1 <= middle) && (index2 <= last)) {
             if (arr[index1] < arr[index2]) {
                 temp[index] = arr[index1];
                 index1++;
@@ -48,11 +78,15 @@ public class Sorts {
             index++;
             index1++;
         }
-        while (index2 <= right) {
+        while (index2 <= last) {
             temp[index] = arr[index2];
             index++;
             index2++;
         }
+        for (int i = first; i < last; i++) {
+            arr[i] = temp[i - first];    
+        } 
+        */
     }
 
     public static void quickSort (int[] arr, int N) {
