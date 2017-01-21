8ff75809b476c849786a5ab46327988c9c66293d
agrazela14 <agrazela@gmail.com>
2017-01-20 18:38:07 -0800

    Sort Times class made

--- Lab1/SortTimes.java
+++ Lab1/SortTimes.java
@@ -1,30 +1,83 @@
 import java.util.*;
 
-private static final int MIN 5000
-private static final int MAX 160000
-
 public class SortTimes {
+
+    private static final int MIN = 5000;
+    private static final int MAX = 160000;
+    private static final long MILLI = 1000000; 
 
     public static void main(String args[]) {
         int N;
+        int[] selectionArr = new int[MAX];
+        int[] mergeArr = new int[MAX];
+        int[] quickArr = new int[MAX];
+        long selectionTime;
+        long mergeTime;
+        long quickTime;
+        
         for (N = MIN; N <= MAX; N *= 2) {
             for (int i = 0; i < 5; i++) {
-                runSelectionSort();
-                runMergeSort();
-                runQuickSort();
+                fillArrays(N, selectionArr, mergeArr, quickArr);
+                selectionTime = runSelectionSort(selectionArr, N);
+                mergeTime = runMergeSort(mergeArr, N);
+                quickTime = runQuickSort(quickArr, N);
+                printify(N, i, selectionTime, mergeTime, quickTime);
             }
         }
     }
 
-    private static void runSelectionSort() {
+    private static void fillArrays(int N, int[] sel, int[] merge, int[] quick) {
+        Random rnd = new Random();
+        int rndint;
+
+        for (int i = 0; i < N; i++) {
+            rndint = rnd.nextInt(N);
+            sel[i] = rndint;
+            merge[i] = rndint;
+            quick[i] = rndint;
+        }
+    }
+
+    private static long runSelectionSort(int[] sel, int N) {
+        long pretime;
+        long posttime;
+
+        pretime = System.nanoTime();
+        Sorts.selectionSort(sel, N);
+        posttime = System.nanoTime();
+        
+        return ((posttime - pretime) / MILLI);
+        
+    }
+
+    private static long runMergeSort(int[] merge, int N) {
+        long pretime;
+        long posttime;
+
+        pretime = System.nanoTime();
+        Sorts.mergeSort(merge, N);
+        posttime = System.nanoTime();
+        
+        return ((posttime - pretime) / MILLI);
+        
+    }
+
+    private static long runQuickSort(int[] quick, int N) {
+        long pretime;
+        long posttime;
+
+        pretime = System.nanoTime();
+        Sorts.quickSort(quick, N);
+        posttime = System.nanoTime();
+        
+        return ((posttime - pretime) / MILLI);
 
     }
 
-    private static void runMergeSort() {
-
-    }
-
-    private static void runQuickSort() {
-
+    private static void printify(int N, int i, long sel, long merge, long quick) {
+        System.out.println("N = " + N + " T_ss = " + sel + " T_ms = " + merge + " T_qs = " + quick);
+        if (i == 5) {
+            System.out.println();
+        } 
     }
 }
