225f5b0ab94aafa339ffb217eb5314bbf7336d2a
agrazela14 <agrazela@gmail.com>
2017-02-15 12:08:18 -0800

    2/16

--- Lab3/FactoryProblem.java
+++ Lab3/FactoryProblem.java
@@ -1,18 +1,19 @@
-import java.util.*
-import java.io.*
-import java.lang.*
+import java.util.*;
+import java.io.*;
+import java.lang.*;
 
-
-public class FactorProblem {
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in), fileReader;
+public class FactoryProblem {
+    public static void main(String args[]) throws FileNotFoundException {
+        Scanner in = new Scanner(System.in);
         String filename;
         int rowsA, rowsB, colsA, colsB;
         int S[][], T[][], min[][], j, e1, e2, x1, x2;
 
         System.out.println("Enter in the input filename");
         filename = in.nextLine();
-        fileReader = new Scanner(new File(filename));
+
+        Scanner fileReader = new Scanner(new File(filename));
+
         j = fileReader.nextInt();
         e1 = fileReader.nextInt();
         e2 = fileReader.nextInt();
@@ -37,30 +38,55 @@ public class FactorProblem {
             T[1][i] = fileReader.nextInt();
         }
 
-        min = twoLineFactory(min, T, S, e1, e2, x1, x2, j);
-        printResults(min, x1, x2);
+        twoLineFactory(T, S, e1, e2, x1, x2, j);
     }
 
-    private static int[][] twoLineFactory(int[][] T, int[][] S, int e1, int e2, int x1, int x2, int j) {
-        int [][] min = new int[2][j];
+    private static void twoLineFactory(int[][] T, int[][] S, int e1, int e2, int x1, int x2, int j) {
+        int [][] min = new int[2][j + 1];
+        int [][] route = new int[2][j + 1];
         min[0][0] = e1 + S[0][0]; 
         min[1][0] = e2 + S[1][0]; 
 
         for (int i = 1; i < j; i++) {
-            min[0][i] = compare(min[0][i - 1] + S[0][i], min[1][i - 1] + T[1][i - 1] + S[0][i]);
-            min[1][i] = compare(min[1][i - 1] + S[1][i], min[0][i - 1] + T[0][i - 1] + S[1][i]);
+            min[0][i] = compare(min[0][i - 1] + S[0][i], min[1][i - 1] + T[1][i - 1] + S[0][i], route, i - 1, 0);
+            min[1][i] = compare(min[0][i - 1] + T[0][i - 1] + S[1][i], min[1][i - 1] + S[1][i], route, i - 1, 1);
         }
-        return min;
+        min[0][j] = min[0][j - 1] + x1;
+        min[1][j] = min[1][j - 1] + x2;
+
+        printResults(min, route);
     }
-
-
-    private static int compare(int x, int y) {
-        if (x > y) {
+    //route array 1 column 0s and 1s for which way it went
+    //Other column is just index
+    private static int compare(int x, int y, int[][] route, int index, int lineNum) {
+        if (x < y) {
+            route[lineNum][index] = 0; 
             return x;
         }
+        route[lineNum][index] = 1; 
         return y;
     }
 
-    print
+    private static void printResults(int[][] min, int[][] route) {
+        int j = min[0].length - 1;
+        int routeLine = (min[0][j] < min[1][j]) ? 0 : 1;
+        int optimalTime = (min[0][j] < min[1][j]) ? min[0][j] : min[1][j];
+        int[] LineNumbers = new int[j];
+        for (int i = j - 1; i > 0; i--) {
+            if (route[routeLine][i] == 0) {
+                LineNumbers[j - (i + 1)] = 1;
+                routeLine = 0;
+            }
+            else {
+                LineNumbers[j - (i + 1)] = 2;
+                routeLine = 1;
+            }
+        }
+        System.out.println("The optimal solution is " + optimalTime + "\n");
+        System.out.println("The optimal route is:");
 
+        for (int i = 0; i < LineNumbers.length; i++) {
+            System.out.println("station " + (i + 1) + ", line " + LineNumbers[i]);
+        }
+    }
 }
